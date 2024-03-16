f07223a8968681e4b04e526f58bbf14f056ea7e0
arch-adi21 <22f3001792@ds.study.iitm.ac.in>
2024-03-17 01:30:08 +0530

    component for data ingestion created pipeline for data ingestion created main.py updated for data ingestion task datasets added to gitignore files

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.step_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Testing the custom logger")
+STEP_ = "Data Ingestion"
+
+try:
+    logger.info(f"{STEP_} started already ...")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{STEP_} completed successfully \n<== ==== ==== === ==== ==== ==>")
+except Exception as e:
+    logger.error(f"{STEP_} failed with error: {e}")
+    raise e
