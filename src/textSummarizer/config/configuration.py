207e125dba3fb0df512a2dcd5e30a31f69a2ed9f
arch-adi21 <22f3001792@ds.study.iitm.ac.in>
2024-04-22 22:28:17 +0530

    added entity and config_manager for model training

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -52,3 +52,26 @@ class ConfigurationManager :
                 tokenizer_name = config.tokenizer_name
           )
           return data_transformation_config
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.TrainingArguments
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_ckpt = config.model_ckpt,
+            num_train_epochs = params.num_train_epochs,
+            warmup_steps = params.warmup_steps,
+            per_device_train_batch_size = params.per_device_train_batch_size,
+            weight_decay = params.weight_decay,
+            logging_steps = params.logging_steps,
+            evaluation_strategy = params.evaluation_strategy,
+            eval_steps = params.eval_steps,
+            save_steps = params.save_steps,
+            gradient_accumulation_steps = params.gradient_accumulation_steps
+        )
+
+        return model_trainer_config
