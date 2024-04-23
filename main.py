f0af622f96e212a5e4274bc62f40319447f45dd2
arch-adi21 <22f3001792@ds.study.iitm.ac.in>
2024-04-23 12:06:57 +0530

    Updated main method with model evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textSummarizer.pipeline.step_01_data_ingestion import DataIngestionTraining
 from textSummarizer.pipeline.step_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.step_03_data_transformation import DataTransformationTrainingPipeline
 from textSummarizer.pipeline.step_04_model_trainer import ModelTrainerPipeline
+from textSummarizer.pipeline.step_05_model_evaluation import ModelEvaluationPipeline
 from textSummarizer.logging import logger
 
 STEP_ = "Data Ingestion"
@@ -40,6 +41,16 @@ try:
     logger.info(f"{STEP_} started already...")
     model_trainer = ModelTrainerPipeline()
     model_trainer.main()
+    logger.info(f"{STEP_} completed successfully \n<== ==== ==== ==== ==== ==== ==>")
+except Exception as e:
+    logger.error(f"{STEP_} failed with error: {e}")
+    raise e
+
+STEP_ = "Model Evaluation"
+try:
+    logger.info(f"{STEP_} started already...")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f"{STEP_} completed successfully \n<== ==== ==== ==== ==== ==== ==>")
 except Exception as e:
     logger.error(f"{STEP_} failed with error: {e}")
