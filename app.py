9218a4061ae605317c161823c1e4d7cfe3ab84fd
arch-adi21 <22f3001792@ds.study.iitm.ac.in>
2024-04-23 12:18:35 +0530

    created prediction pipeline

---
+++ app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import uvicorn
+import sys
+import os
+from fastapi.templating import Jinja2Templates
+from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from textSummarizer.pipeline.step_06_prediction import PredictionPipeline
