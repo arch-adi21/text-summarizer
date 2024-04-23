c12a6040065d45481e20b3213223a498506e9d62
arch-adi21 <22f3001792@ds.study.iitm.ac.in>
2024-04-23 12:00:02 +0530

    added configuration manager for model evaluation

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -4,6 +4,7 @@ from textSummarizer.entity import DataIngestionConfig
 from textSummarizer.entity import DataValidationConfig
 from textSummarizer.entity import DataTransformationConfig
 from textSummarizer.entity import ModelTrainerConfig
+from textSummarizer.entity import ModelEvaluationConfig
 
 class ConfigurationManager :
     def __init__(
@@ -76,3 +77,19 @@ class ConfigurationManager :
         )
 
         return model_trainer_config
+    
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            model_path = config.model_path,
+            tokenizer_path = config.tokenizer_path,
+            metric_file_name = config.metric_file_name
+           
+        )
+
+        return model_evaluation_config
