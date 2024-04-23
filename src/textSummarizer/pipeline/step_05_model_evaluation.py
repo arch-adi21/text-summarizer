d19cbc324e8e55213553f40d65316844f70c5721
arch-adi21 <22f3001792@ds.study.iitm.ac.in>
2024-04-23 12:05:30 +0530

    added pipeline and components for model evaluation

---
+++ src/textSummarizer/pipeline/step_05_model_evaluation.py
@@ -0,0 +1,13 @@
+from textSummarizer.components.model_evaluation import ModelEvaluation
+from textSummarizer.logging import logger
+from textSummarizer.config.configuration import ConfigurationManager
+
+class ModelEvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        model_evaluation_config = config.get_model_evaluation_config()
+        model_evaluation_config = ModelEvaluation(config=model_evaluation_config)
+        model_evaluation_config.evaluate()
